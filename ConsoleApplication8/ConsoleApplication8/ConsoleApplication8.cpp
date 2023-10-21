0430a8898f3606e34067cd159b6cd2da8e55ef82
reigadegr <2722688642@qq.com>
2023-10-21 10:25:35 +0800

    update in Windows

--- ConsoleApplication8/ConsoleApplication8/ConsoleApplication8.cpp
+++ ConsoleApplication8/ConsoleApplication8/ConsoleApplication8.cpp
@@ -2,35 +2,38 @@
 #include <vector>
 #include"LOG.hpp"
 #include "pd.h"
+auto pd(std::string word, std::string input, int &error_sum) -> bool;
 struct td {
 	std::string tm;
 	std::string xx;
 	std::string da;
 };
 int error_sum = 0;
-void runMain(std::vector<td> &wt);
-void runMain(std::vector<td> &wt) {
-	//#include "u1.h"
-	#include "u2.h"
+auto runMain(std::vector<td> &wt)->void {
 
-	for (const auto& word : wt) {
-		if (word.tm == "" || word.da == "" || word.xx == "")
-		{
-			continue;
-		}
-		LOG("题目：", word.tm);
-		LOG("\n", word.xx, "\n");
-		std::string input;
-		std::cin >> input;
-		pd(word.da, input, error_sum);
-	}
-}
+//#include "u1.h"
+//#include "u2.h"
+#include "u3.h"
+
+    for (const auto &word : wt) {
+        
+        if (word.tm == "" || word.da == "" || word.xx == "") {
+            continue;
+        }
+        LOG("题目：", word.tm);
+        LOG("\n", word.xx, "\n");
+        std::string input;
+        std::cin >> input;
+        pd(word.da, input, error_sum);
+    };
+};
 
 
 auto main(int argc ,char **argv)->int {
-    LOG("全部输入小写字母");
+    LOG("全部输入小写字母。输入666可以查看当前题目答案");
 	std::vector<td> wt;
 	runMain(wt);
+    LOG("错误次数：", error_sum);
     LOG("测试已结束");
     std::cin.get();
     std::cin.get();
---
+++ ConsoleApplication8/ConsoleApplication8/u3.h
@@ -0,0 +1,35 @@
+#pragma once
+
+
+wt.push_back({"听力题：", "A. She is suffering a pain in her neck.\nB. She is likely to replace Miss Smith.\nC. She has to do extra work for a few days.\nD. She is quite sick of working overtime.", "c"});
+
+
+wt.push_back({"听力题：", "A. He forgot where he had left the package.\nB. He slipped on his way to the post office.\nC. He wanted to deliver the package himself.\nD. He failed to do what he promised to do.", "d"});
+
+wt.push_back({"听力题：", "A. The woman is just as unlucky as the man. \nB. The woman is more sensitive than the man.  \nC. The speakers share a common view on love.\nD. The speakers are unhappy with their marriage.", "c"});
+
+wt.push_back({"听力题：", "A. Preparations for a forum. \nB. Preparations for an interview.\nC. Preparations for a banquet.\nD. Preparations for making chairs.", "a"});
+
+wt.push_back({"听力题：", "A. He will give the woman some tips on the game.\nB. The woman has good reason to quit the game.\nC. He is willing to play chess with the woman.\nD. The woman should go on playing chess.", "d"});
+wt.push_back({"听力题：", "A. The Internet could be very confusing.\nB. Many people reported their QQ IDs were stolen.\nC. A computer with a firewall was safe.\nD. Many people reported they could not log on to their QQ.", "b"});
+wt.push_back({"听力题：", "A. Trojan Horse can steal password information.\nB. Viruses can download a user’s personal information.\nC. Viruses link everyone together.\nD. Viruses are unavoidable.", "b"});
+wt.push_back({"听力题：", "A. The Internet makes his life more convenient.\nB. The Internet is safe all the time.\nC. The Internet links everyone together.\nD. The Internet itself can give out personal information.", "c"});
+wt.push_back({"听力题：", "A. Turn to the law for help.\nB. There is nothing he could do.\nC. Try to find out who did it.\nD. Buy a new computer and get a new QQ ID.", "a"});
+wt.push_back({"听力题：", "A. Do not reveal personal information on the Internet at will.\nB. Use a secure password.\nC. Do not access any fishing sites.\nD. All of above.", "d"});
+wt.push_back({"听力题：", "A. In May and July, respectively.\nB. In June and May, respectively.\nC. In July and September, respectively.\nD. In May and June, respectively.", "d"});
+wt.push_back({"听力题：", "A. Ann Jarvis and Virginia.\nB. Ann Jarvis and Mrs. Dodd.\nC. William Smart and Mrs. Dodd.\nD. Todd and William.", "b"});
+wt.push_back({"听力题：", "A. She died on the second Sunday in May, 1905.\nB. She died on the third Sunday in May, 1909.\nC. She died on the second Sunday in May, 1910.\nD. She died on the third Sunday in May, 1905.", "a"});
+wt.push_back({"听力题：", "A. Her father died early.\nB. Her father was greater than her mother.\nC. Her father should be honored because he raised the family.\nD. Her father loved her the most.", "c"});
+wt.push_back({"听力题：", "A. The two Days are celebrated in various ways.\nB. Many people go to churches or other religious events.\nC. Families gather together to celebrate the two Days.\nD. People buy their parents valuable gifts.", "d"});
+
+wt.push_back({"听力题：", "A. People use words and phrases to communicate.\nB. The meanings of some words and phrases may be different in different cultures.\nC. Not knowing such difference may lead to misunderstandings.\nD. The meaning of a word or phrase has nothing to do with culture.", "d"});
+wt.push_back({"听力题：", "A. The mistake the speaker’s English friends made in Holland.\nB. The mistake the speaker’s Spanish friends made in England.\nC. The mistake the speaker’s English friends made in France.\nD. The mistake the speaker’s Spanish friends made in Holland.", "b"});
+wt.push_back({"听力题：", "A. It means “fun”.\nB. It means that the driver should take another way to avoid the construction.\nC. It means “a hole”.\nD. It means “danger”.", "a"});
+wt.push_back({"听力题：", "A. They ended in a large hole.\nB. One of them dropped their new Gucci watch while pushing the car free. \nC. It took them hours to search the hole before they finally found the watch.\nD. All the above.", "d"});
+wt.push_back({"听力题：", "A. In French, “Thank you” can be used to reject an offer of food or drink.\nB. In French, “Thank you” can be used to accept an offer of food or drink.\nC. In French, “Thank you” is different from “no, thank you” in English.\nD. In French, “Thank you” can be used when one wants another cup of coffee.", "a"});
+wt.push_back({"听力题：", "", ""});
+wt.push_back({"听力题：", "", ""});
+wt.push_back({"听力题：", "", ""});
+wt.push_back({"听力题：", "", ""});
+wt.push_back({"听力题：", "", ""});
+wt.push_back({"听力题：", "", ""});
